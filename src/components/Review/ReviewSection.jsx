db219ba8d9fdca2d25f4e33ec31589f5def01499
Thumin <96730005+Thu-Min@users.noreply.github.com>
2024-06-05 10:36:24 +0700

    fix: link in review

--- src/components/Review/ReviewSection.jsx
+++ src/components/Review/ReviewSection.jsx
@@ -5,32 +5,44 @@ const ReviewSection = () => {
         <>
             <div className="md:mx-[100px] md:p-[75px] md:space-y-10 sm:mx-[25px] sm:p-[25px] sm:space-y-10">
                 <div className="md:flex md:justify-around sm:grid sm:grid-cols-3 sm:place-items-start sm:place-content-center sm:gap-10">
-                    <div className="flex flex-col items-center w-[100px]">
-                        <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
-                        <label htmlFor="" className="text-center">Venus</label>
-                    </div>
-                    <div className="flex flex-col items-center w-[100px]">
-                        <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
-                        <label htmlFor="" className="text-center">Wedding Package</label>
-                    </div>
-                    <div className="flex flex-col items-center w-[100px]">
-                        <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
-                        <label htmlFor="" className="text-center">Catering</label>
-                    </div>
-                    <div className="flex flex-col items-center w-[100px]">
-                        <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
-                        <label htmlFor="" className="text-center">Photography Videography</label>
-                    </div>
-                    <div className="flex flex-col items-center w-[100px]">
-                        <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
-                        <label htmlFor="" className="text-center">Floral Design</label>
-                    </div>
-                    <div className="flex flex-col items-center w-[100px]">
-                        <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
-                        <label htmlFor="" className="text-center">Make-Up & Stylish Team</label>
-                    </div>
+                    <a href="#venue">
+                        <div className="flex flex-col items-center w-[100px]">
+                            <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
+                            <label htmlFor="" className="text-center">Venus</label>
+                        </div>
+                    </a>
+                    <a href="#weddingpackage">
+                        <div className="flex flex-col items-center w-[100px]">
+                            <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
+                            <label htmlFor="" className="text-center">Wedding Package</label>
+                        </div>
+                    </a>
+                    <a href="#catering">
+                        <div className="flex flex-col items-center w-[100px]">
+                            <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
+                            <label htmlFor="" className="text-center">Catering</label>
+                        </div>
+                    </a>
+                    <a href="#photography">
+                        <div className="flex flex-col items-center w-[100px]">
+                            <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
+                            <label htmlFor="" className="text-center">Photography Videography</label>
+                        </div>
+                    </a>
+                    <a href="#floral">
+                        <div className="flex flex-col items-center w-[100px]">
+                            <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
+                            <label htmlFor="" className="text-center">Floral Design</label>
+                        </div>
+                    </a>
+                    <a href="#makup">
+                        <div className="flex flex-col items-center w-[100px]">
+                            <div className="p-5 bg-[#F27083] w-10 rounded-full"></div>
+                            <label htmlFor="" className="text-center">Make-Up & Stylish Team</label>
+                        </div>
+                    </a>   
                 </div>
-                <div>
+                <div id="venue">
                     <h2 className="md:text-[38px] my-5 sm:text-[26px]">Most Popular Venues</h2>
                     <div className="md:grid md:grid-cols-4 sm:grid-cols-1">
                         <div className="bg-white md:w-[270px] sm:w-full rounded">
@@ -67,7 +79,7 @@ const ReviewSection = () => {
                         </div>
                     </div>
                 </div>
-                <div>
+                <div id="weddingpackage">
                     <h2 className="md:text-[38px] my-5 sm:text-[26px]">Most Popular Venues</h2>
                     <div className="md:grid md:grid-cols-4 sm:grid-cols-1">
                         <div className="bg-white md:w-[270px] sm:w-full rounded">
@@ -104,7 +116,7 @@ const ReviewSection = () => {
                         </div>
                     </div>
                 </div>
-                <div>
+                <div id="catering">
                     <h2 className="md:text-[38px] my-5 sm:text-[26px]">Most Popular Venues</h2>
                     <div className="md:grid md:grid-cols-4 sm:grid-cols-1">
                         <div className="bg-white md:w-[270px] sm:w-full rounded">
@@ -141,7 +153,7 @@ const ReviewSection = () => {
                         </div>
                     </div>
                 </div>
-                <div>
+                <div id="photography">
                     <h2 className="md:text-[38px] my-5 sm:text-[26px]">Most Popular Venues</h2>
                     <div className="md:grid md:grid-cols-4 sm:grid-cols-1">
                         <div className="bg-white md:w-[270px] sm:w-full rounded">
@@ -178,7 +190,44 @@ const ReviewSection = () => {
                         </div>
                     </div>
                 </div>
-                <div>
+                <div id="floral">
+                    <h2 className="md:text-[38px] my-5 sm:text-[26px]">Most Popular Venues</h2>
+                    <div className="md:grid md:grid-cols-4 sm:grid-cols-1">
+                        <div className="bg-white md:w-[270px] sm:w-full rounded">
+                            <img className="w-full md:h-[200px] sm:h-full object-cover rounded" src={beach} alt="" />
+                            <div className="md:mx-3 md:my-2 sm:mx-3 sm:my-5">
+                                <h3 className="md:text-[18px] sm:text-[24px]">Beach</h3>
+                                <h3 className="md:text-[18px] sm:text-[24px]">Location</h3>
+                                <h3 className="md:text-[18px] sm:text-[24px]">$500</h3>
+                            </div>
+                        </div>
+                        <div className="bg-white md:w-[270px] sm:w-full rounded">
+                            <img className="w-full md:h-[200px] sm:h-full object-cover rounded" src={beach} alt="" />
+                            <div className="md:mx-3 md:my-2 sm:mx-3 sm:my-5">
+                                <h3 className="md:text-[18px] sm:text-[24px]">Beach</h3>
+                                <h3 className="md:text-[18px] sm:text-[24px]">Location</h3>
+                                <h3 className="md:text-[18px] sm:text-[24px]">$500</h3>
+                            </div>
+                        </div>
+                        <div className="bg-white md:w-[270px] sm:w-full rounded">
+                            <img className="w-full md:h-[200px] sm:h-full object-cover rounded" src={beach} alt="" />
+                            <div className="md:mx-3 md:my-2 sm:mx-3 sm:my-5">
+                                <h3 className="md:text-[18px] sm:text-[24px]">Beach</h3>
+                                <h3 className="md:text-[18px] sm:text-[24px]">Location</h3>
+                                <h3 className="md:text-[18px] sm:text-[24px]">$500</h3>
+                            </div>
+                        </div>
+                        <div className="bg-white md:w-[270px] sm:w-full rounded">
+                            <img className="w-full md:h-[200px] sm:h-full object-cover rounded" src={beach} alt="" />
+                            <div className="md:mx-3 md:my-2 sm:mx-3 sm:my-5">
+                                <h3 className="md:text-[18px] sm:text-[24px]">Beach</h3>
+                                <h3 className="md:text-[18px] sm:text-[24px]">Location</h3>
+                                <h3 className="md:text-[18px] sm:text-[24px]">$500</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="makup">
                     <h2 className="md:text-[38px] my-5 sm:text-[26px]">Most Popular Venues</h2>
                     <div className="md:grid md:grid-cols-4 sm:grid-cols-1">
                         <div className="bg-white md:w-[270px] sm:w-full rounded">
